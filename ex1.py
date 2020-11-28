8b3cf6b3e5cef05777060f61bcefff172256bde7
Orna Lipkin <fifilp@gmail.com>
2020-11-28 09:03:56 +0200

    checking table list first and returning the list if valid. comparing the list to "From part" and "where part"

--- ex1.py
+++ ex1.py
@@ -3,17 +3,22 @@ def is_valid_table(i_table):
     return (table == "customers" or table == "orders")
 
 
-def is_valid_attribute(i_attribute):
+def is_valid_attribute(i_attribute, i_tables):
     attribute = i_attribute.strip()
-    return (attribute == "customers.name" or attribute == "customers.age" 
-    or attribute == "orders.customername" or attribute == "orders.product" or attribute == "orders.price")
+    result = False
+    for table in i_tables: 
+        if(attribute.startswith(table)):
+            result = (attribute == "customers.name" or attribute == "customers.age" 
+                    or attribute == "orders.customername" or attribute == "orders.product" or attribute == "orders.price")
+
+    return result
 
 
-def is_valid_constant(i_constant):    
+def is_valid_constant(i_constant, i_tables):    
     constant = i_constant.strip()
     result = False
   
-    if(is_valid_attribute(constant)):
+    if(is_valid_attribute(constant, i_tables)):
         result = True
     elif(constant.isnumeric()):
         result = True
@@ -46,7 +51,7 @@ def find_valid_operator(i_simple_condition):
     return result
 
 
-def is_valid_simple_condition(i_simple_condition):
+def is_valid_simple_condition(i_simple_condition, i_tables):
     simple_condition = i_simple_condition.strip()
 
     operator = find_valid_operator(simple_condition)
@@ -54,27 +59,27 @@ def is_valid_simple_condition(i_simple_condition):
         result = False
     else:        
         parts_array = simple_condition.split(operator)        
-        result = is_valid_constant(parts_array[0]) and is_valid_constant(parts_array[1])
+        result = is_valid_constant(parts_array[0], i_tables) and is_valid_constant(parts_array[1], i_tables)
 
     return result
 
 
-def is_valid_att_list(i_att_list):
+def is_valid_att_list(i_att_list, i_tables):
     att_list = i_att_list.strip()
 
-    if(is_valid_attribute(att_list)):
+    if(is_valid_attribute(att_list, i_tables)):
         result = True
     else:
         comma_index = att_list.find(",")
         if(comma_index == -1):
             result = False
         else:
-            result = is_valid_attribute(att_list[0:comma_index]) and is_valid_att_list(att_list[comma_index+1:])
+            result = is_valid_attribute(att_list[0:comma_index], i_tables) and is_valid_att_list(att_list[comma_index+1:], i_tables)
     
     return result
 
 
-def is_valid_attribute_list(i_attribute_list):
+def is_valid_attribute_list(i_attribute_list, i_tables):
     attribute_list = i_attribute_list.strip()
     index_of_astrix = attribute_list.find("*")
 
@@ -84,7 +89,7 @@ def is_valid_attribute_list(i_attribute_list):
         else:
             result = False
     else:
-        result = is_valid_att_list(attribute_list)
+        result = is_valid_att_list(attribute_list,i_tables)
     
     return result
 
@@ -104,11 +109,11 @@ def is_valid_table_list(i_table_list):
     return result   
 
 
-def is_valid_condition(i_condition):
+def is_valid_condition(i_condition, i_tables):
     condition = i_condition.strip()
     result = False
 
-    if(is_valid_simple_condition(condition)):
+    if(is_valid_simple_condition(condition, i_tables)):
         result = True    
     else:
         checked_all_options = False
@@ -120,14 +125,14 @@ def is_valid_condition(i_condition):
             if(and_index != -1):
                 left_and_part = condition[0:and_index]
                 right_and_part = condition[and_index+5:]
-                if(is_valid_condition(left_and_part) and is_valid_condition(right_and_part)):
+                if(is_valid_condition(left_and_part, i_tables) and is_valid_condition(right_and_part, i_tables)):
                     result=True                    
                 else:
                     and_index = condition.find(" and ", and_index+5)
             elif(or_index != -1):
                 left_or_part = condition[0:or_index]
                 right_or_part = condition[or_index+4:]
-                if(is_valid_condition(left_or_part) and is_valid_condition(right_or_part)):
+                if(is_valid_condition(left_or_part, i_tables) and is_valid_condition(right_or_part, i_tables)):
                     result=True                    
                 else:
                     or_index = condition.find(" or ", or_index+4)
@@ -136,12 +141,12 @@ def is_valid_condition(i_condition):
 
         if (not result):
             if(condition[0] == "(" and condition[-1] == ")"):                
-                result = is_valid_condition(condition[1:-1])
+                result = is_valid_condition(condition[1:-1], i_tables)
 
     return result
 
 
-def is_select_part_valid(i_select_part):
+def is_select_part_valid(i_select_part, i_tables):
     result = False
     # i_select_part starts with "select" and there has to be a space right after it
     if (i_select_part[6] == " "):   
@@ -149,27 +154,38 @@ def is_select_part_valid(i_select_part):
         distinct_index = select_part.find("distinct")
         if(distinct_index == 0):
             select_part = select_part[8:]   # attribute list starts after the distinct          
-        result = is_valid_attribute_list(select_part)  
+        result = is_valid_attribute_list(select_part, i_tables)  
 
     return result
 
 
-def is_from_part_valid(i_from_part):
+def decipher_table_list(i_from_part):
+    result = i_from_part.split(",")
+    for table in result:
+        table = table.strip()
+    
+    return result
+
+#returns a tuple: 1st element-bool and 2nd element-a list of tables.
+def is_from_part_valid(i_from_part):    
     result = False
+    table_list = None
+
     # i_from_part starts with "from" and there has to be a space right after it  
     if(i_from_part[4] == " "):
         from_part = i_from_part[4:].strip()
         result = is_valid_table_list(from_part)
+        if(result):
+            table_list=decipher_table_list(from_part)
 
-    return result
+    return (result, table_list)
 
-
-def is_where_part_valid(i_where_part):
+def is_where_part_valid(i_where_part, i_tables):
     result = False        
     # i_where_part starts with "where" and there has to be a space right after it   
     if(i_where_part[5] == " "):
         where_part = i_where_part[5:].strip()
-        result = is_valid_condition(where_part)    
+        result = is_valid_condition(where_part, i_tables)    
 
     return result
 
@@ -181,17 +197,20 @@ def is_valid_query(i_query):
     
     if(query[-1]==";"):
         query=query[0:-1]
+        
         select_index = query.find("select")     
         from_index = query.find("from")
-          
-        if(not is_select_part_valid(query[select_index:from_index])):
-            result = "Invalid. Parsing <attribute_list> failed"
-        else:
-            where_index = query.find("where")
-            if(not is_from_part_valid(query[from_index:where_index])):
-                result = "Invalid. Parsing <table_list> failed"
+        where_index = query.find("where")
+
+        (from_result, table_list) = is_from_part_valid(query[from_index:where_index])
+
+        if(not from_result):
+            result = "Invalid. Parsing <table_list> failed"
+        else:            
+            if(not is_select_part_valid(query[select_index:from_index],table_list)):
+                result = "Invalid. Parsing <attribute_list> failed"
             else:
-                if(not is_where_part_valid(query[where_index:])):
+                if(not is_where_part_valid(query[where_index:], table_list)):
                     result = "Invalid Parsing <condition> failed"
     else:   #missing ; at the end of the query
        result = "Invalid Parsing <condition> failed"
